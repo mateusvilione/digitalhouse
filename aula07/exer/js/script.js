449b752557eec6ce1c247f3f71a93eb08f2b6bc2
Mateus Vilione <mateusvilione@gmail.com>
2020-07-27 20:50:33 -0300

    aula07

---
+++ aula07/exer/js/script.js
@@ -0,0 +1,57 @@
+// Exer 1
+let inteiroPositivo = 5;
+let inteiroNegatigo = -5;
+let numeroDecimal = 10.5;
+let texto = "Hello World!!!";
+let ehVerdadeiro = true;
+
+// Exer 2
+console.log(inteiroPositivo);
+console.log(inteiroNegatigo);
+console.log(numeroDecimal);
+console.log(texto);
+console.log(ehVerdadeiro);
+
+// Exer 3
+
+let nome = "Mateus";
+let sobreNome = "Vilione";
+
+console.log(nome + " " + sobreNome);
+
+// Exer 4
+/*
+var nome = "Clara";
+console.log(Nome);
+    Erro, pois a variável Nome é diferente de nome.
+*/
+
+// Exer 5
+/*
+var nome = "Clara";
+console.log(nome, sobrenome);
+    Error, a variável não foi definida (sobrenome is not defined)
+*/
+
+// Exer 6
+/*
+console.log("teste 
+
+de enter");
+*/
+
+// Exer 7
+
+let valorNulo = null;
+let naodefinido = undefined;
+
+console.log(valorNulo);
+console.log(naodefinido);
+
+// Exer 8
+
+let arrayFruta = ["Açaí", "Acerola", "Amora", "Bacaba", "Banana"];
+console.log(arrayFruta);
+
+// Exer 9
+let frase = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar  e criar o que sempre sonharam."
